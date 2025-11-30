9463f998feb03158837948c0f42485bd378141ca
Muneeswaran <ideazdevelop27@gmail.com>
2025-11-30 23:24:23 +0530

    Update page.js

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -94,7 +94,7 @@ export default function Contact() {
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>
             </span>
             <div>
-              <h4 className="font-semibold mb-0">Email</h4>
+              <h4 className="font-semibold mb-0">Email -</h4>
               <p className="mb-0 text-gray-300">hr@ideaz.org.in</p>
             </div>
           </div>
